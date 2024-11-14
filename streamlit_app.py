074a01c7a4811144cc98c26128d6c120df0feb34
Adonis Cedeño <ajcedeno@espe.edu.ec>
2024-11-14 20:18:56 +0000

    tutorial conectar psql intento 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM mytable;', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
